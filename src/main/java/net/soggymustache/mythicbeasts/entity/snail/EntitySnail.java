62a67f543636cc3a8180cd19749aab1cf99fb2d8
seanamor <frenden@Sean-Amor.local>
2017-02-10 12:00:54 -0600

    Revised
    Revised

--- src/main/java/net/soggymustache/mythicbeasts/entity/snail/EntitySnail.java
+++ src/main/java/net/soggymustache/mythicbeasts/entity/snail/EntitySnail.java
@@ -4,63 +4,92 @@ import java.util.Random;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
-import net.minecraft.entity.ai.EntityAILeapAtTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMoveTowardsTarget;
-import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
-import net.minecraft.entity.ai.EntityMoveHelper;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.pathfinding.PathNavigate;
 import net.minecraft.init.Items;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 import net.minecraft.block.Block;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.datafix.DataFixer;
-
 import net.minecraft.world.World;
-import java.util.HashMap;
+import net.soggymustache.mythicbeasts.init.achieve.MythicAchievements;
+import net.soggymustache.mythicbeasts.init.items.MythicItems;
 
-public class EntitySnail extends EntityLiving implements IMob {
+public class EntitySnail extends EntityAnimal{
     
     public EntitySnail(World worldIn) {
         super(worldIn);
         this.setSize(0.5F, 0.5F);
+        this.tasks.addTask(1, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(3, new EntityAILookIdle(this));
+        this.tasks.addTask(4, new EntityAISwimming(this));
+        this.tasks.addTask(5, new EntityAIPanic(this, 0.38F));
+        this.tasks.addTask(6, new EntityAIAvoidEntity(this, EntityPlayer.class, 8.0F, 0.3F, 0.35F));
+        this.targetTasks.addTask(7, new EntityAIHurtByTarget(this, false, new Class[0]));
+        
     }
-    
+
     @Override
-    protected void applyEntityAttributes()
-    {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(3.0D);
+    public boolean processInteract(EntityPlayer player, EnumHand hand, ItemStack stack) {
+        ItemStack itemstack = player.inventory.getCurrentItem();
+        
+        if(itemstack.getItem() == MythicItems.book_of_beasts){
+            player.addStat(MythicAchievements.achievementTrailblazing);
+        }
+        return super.processInteract(player, hand, stack);
     }
     
     @Override
     public void onLivingUpdate() {
         super.onLivingUpdate();
 
-           if(!this.canTriggerWalking()){
         if(!this.worldObj.isRemote && this.ticksExisted % 5 == 0) {
             Block block = this.worldObj.getBlockState(this.getPosition().add(0, -1, 0)).getBlock();
-            if(block == Blocks.GRASS){
+            if(block == Blocks.GRASS)
                 this.worldObj.setBlockState(this.getPosition().add(0, -1, 0), Blocks.DIRT.getDefaultState());}
-            }
-        }
+    }
+
+    public boolean attackEntityAsMob(Entity entity)
+    {
+        this.worldObj.setEntityState(this, (byte)4);
+
+        return entity.attackEntityFrom(DamageSource.causeMobDamage(this), 0.5F);
+    }
+    
+    @Override
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(2.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.05D);
+    }
+    
+    @Override
+    protected void dropFewItems(boolean wasRecentlyHit, int lootingModifier) {
+        this.dropItem(Items.SLIME_BALL, 1);
+    }
+
+    @Override
+    public EntityAgeable createChild(EntityAgeable ageable) {
+
+        return null;
     }
 }
--- src/main/java/net/soggymustache/mythicbeasts/init/achieve/MythicAchievements.java
+++ src/main/java/net/soggymustache/mythicbeasts/init/achieve/MythicAchievements.java
@@ -7,16 +7,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 public class MythicAchievements {
 	
-	public static Achievement achievementUnseen, achievementShiny, achievementBuzz;
+	public static Achievement achievementUnseen, achievementShiny, achievementBuzz, achievementTrailblazing;
 	
 	
 	public static void init(FMLInitializationEvent event)
 	{
 		achievementUnseen = new Achievement("achievement.Unseen", "Unseen", 0, 1, Items.ENDER_PEARL, (Achievement)null).registerStat();
-		achievementShiny = new Achievement("achievement.Shiny", "Shiny", 1, -2, Items.GOLD_INGOT, (Achievement)null).registerStat();
-		achievementBuzz = new Achievement("achievement.Buzz", "Buzz", 3, -1, Items.ENDER_PEARL, (Achievement)null).registerStat();
+		achievementShiny = new Achievement("achievement.Shiny", "Shiny", 0, 2, Items.ENDER_PEARL, (Achievement)null).registerStat();
+		achievementBuzz = new Achievement("achievement.Buzz", "Buzz", 0, 3, Items.ENDER_PEARL, (Achievement)null).registerStat();
+		achievementTrailblazing = new Achievement("achievement.Trailblazing", "Trailblazing", 0, 4, Items.ENDER_PEARL, (Achievement)null).registerStat();
 
 
-		AchievementPage.registerAchievementPage(new AchievementPage("MythicBeasts", new Achievement[]{achievementUnseen, achievementShiny, achievementBuzz}));
+		AchievementPage.registerAchievementPage(new AchievementPage("MythicBeasts", new Achievement[]{achievementUnseen, achievementShiny, achievementBuzz, achievementTrailblazing}));
 	}
 }
